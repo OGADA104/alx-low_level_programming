9429a246a81d4529b13d38abe29231d7d99589da
OGADA104 <michaelogada104@gmail.com>
2023-03-16 22:02:48 +0000

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*this function prints a string using puts*/
+
+int main(void)
+/*this function prints a string using puts*/
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
