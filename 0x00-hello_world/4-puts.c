f33e53acd36fbadbb7c3ad023e3e1f2bd79fabd4
OGADA104 <michaelogada104@gmail.com>
2023-03-16 21:59:27 +0000

    use puts() to output xters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 /*this function prints a string using puts*/
 
 int main(void)
+/*this function prints a string using puts*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
