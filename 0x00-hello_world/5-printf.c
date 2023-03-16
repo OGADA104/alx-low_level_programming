638b2054776feff105dc9adbdbe61e2eba17b9a8
OGADA104 <michaelogada104@gmail.com>
2023-03-16 22:18:55 +0000

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
-/*this function prints a string using puts*/
-
+/*
+* this function prints a string using puts
+*this function prints a string using puts
+* Description : 'this prigram prints using printf function'
+* @parameter: no parameter
+*/
+{
 int main(void)
-/*this function prints a string using puts
- *Description: 'this prigram prints using printf function'
- *
- * */
+/*
+*this function prints a string using puts
+* Description : 'this prigram prints using printf function'
+* @parameter: no parameter
+*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
