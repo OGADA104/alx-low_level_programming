dfaba0ce3301f9f69c2c60e1fced980e47ca325f
OGADA104 <michaelogada104@gmail.com>
2023-03-17 20:32:52 +0000

    evaluate last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+*main - detemines if random number is positive or negative
+*Return: either positive or negative
+*/
+int main(void)
+{
+	int n;
+	int last_digit = n % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (last_digit > 5)
+		printf("Last digit of %d is greater than 5\n", n);
+	else if (lastt_digit == 0)
+		printf("Last digit of %d and is 0\n", n);
+	else if (last_digit < 5 && != 0)
+		printf("Last digit of %d and is less than 6 and 0\n", n);
+	return (0);
+}
