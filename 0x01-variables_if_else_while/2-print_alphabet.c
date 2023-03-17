b159a823b2b833feacb520004dbda0061ca172c6
OGADA104 <michaelogada104@gmail.com>
2023-03-17 21:18:22 +0000

    putchar small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+*main - prints characters a-z in lowercase
+*Return: 0 if success
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
