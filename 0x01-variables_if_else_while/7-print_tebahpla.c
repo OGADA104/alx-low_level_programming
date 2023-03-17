895e284f2b044a65beaa07ee82929e5532105d2e
OGADA104 <michaelogada104@gmail.com>
2023-03-17 22:31:09 +0000

    print in reverse order ; decreament

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+*main - prints characters z-a in lowercase
+*Return: 0 if success
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; --c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
