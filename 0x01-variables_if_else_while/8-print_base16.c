a988e6dd70d5ead65465cf236b1f711e91a4175f
OGADA104 <michaelogada104@gmail.com>
2023-03-17 22:56:08 +0000

    numbers to base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+*main - prints 1- 10
+*Return: 0 if success
+*/
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
