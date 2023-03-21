656c7af9e79b421d075f1b773ba13f95da31b567
OGADA104 <michaelogada104@gmail.com>
2023-03-21 19:25:19 +0000

    check absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_abs - gets absolute value
+*@n: integer to check
+*Return: n or -n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
