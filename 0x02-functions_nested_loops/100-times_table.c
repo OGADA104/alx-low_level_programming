bfaacd71a6b79e34771a7dfbc30fa707fbf766da
OGADA104 <michaelogada104@gmail.com>
2023-03-22 09:48:01 +0000

    times table for n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,12 +9,10 @@ void print_times_table(int n)
 {
 	int i, j;
 
+	if (n < 0 || n > 15)
+		return;
 	for (i = 0; i <= n; i++)
 	{
-		if (n > 15 || n == 0)
-		{
-			return;
-		}
 		for (j = 0; j <= n; j++)
 		{
 			if (j == 0)
