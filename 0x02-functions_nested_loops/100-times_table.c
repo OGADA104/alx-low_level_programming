51c2157f7aa02b052e322d2eed2344f0e63a567e
OGADA104 <michaelogada104@gmail.com>
2023-03-22 09:41:00 +0000

    times table for n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*print_times_table - prints multiple tables of n
+*@n: intake integer
+*Return: void
+*/
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 15 || n == 0)
+		{
+			break;
+		}
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+				printf("%d", i * j);
+			else
+				printf("%4d", i * j);
+		}
+		printf("\n");
+	}
+}
