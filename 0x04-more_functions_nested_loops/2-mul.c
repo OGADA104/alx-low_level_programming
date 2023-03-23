ef370a8a55f25d55f493a7ecd2c745e1b940b044
OGADA104 <michaelogada104@gmail.com>
2023-03-23 18:18:18 +0000

    multiplication if two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*mul - multiplies inputs
+*@a: first input int
+*@b: second input int
+*Return: returns the results after multiplication
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
