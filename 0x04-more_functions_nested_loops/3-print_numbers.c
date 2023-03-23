df3c3e97217a5f171c98ca228cf401d79840c2a6
OGADA104 <michaelogada104@gmail.com>
2023-03-23 18:38:45 +0000

    print numbers 1 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*print_numbers - print numbers 1-9
+*
+*
+*Return: void
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
