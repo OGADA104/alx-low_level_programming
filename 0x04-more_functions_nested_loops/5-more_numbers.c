de5b51140c93f8f65db0644714f7bb48195cbdc8
OGADA104 <michaelogada104@gmail.com>
2023-03-23 19:29:01 +0000

    print 1-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*more_numbers - prints 1-14 10 times
+*
+*Return: void
+*
+*/
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (num = 0; num < 15; i++)
+		{
+		printf("%d", num);
+		}
+		printf("\n");
+	}
+}
