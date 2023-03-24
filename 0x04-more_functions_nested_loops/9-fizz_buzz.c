b77d666857947679834f76cc335329995e1cad86
OGADA104 <michaelogada104@gmail.com>
2023-03-24 20:33:39 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,26 @@ int main(void)
 		if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
-			printf(" ");
 		}
 		if (i == 100)
 		{
 			printf("\n");
+		}
+		else
+		{
+			printf(" ");
 		}
 	}
 	return (0);
