5143fc744f8437c06d43de0d5a25f877baa133db
OGADA104 <michaelogada104@gmail.com>
2023-03-27 17:59:32 +0000

    print last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+*puts_half - outputs half
+*@str: input string
+*
+*/
+void puts_half(char *str)
+{
+	int i, n, len = strlen(str);
+
+	if (len % 2 != 0)
+	{
+		n = (len - 1)/2;
+	}
+	else 
+		n = len / 2;
+	for (i = n; i <= len; i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
