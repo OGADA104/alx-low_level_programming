0ee6efa34a9c6308c5687bb2c9745248f1e889ef
OGADA104 <michaelogada104@gmail.com>
2023-03-27 17:26:05 +0000

    skip next number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+*puts2 - prints 0,2,4,6,8,...
+*@str: input string
+*/
+void puts2(char *str)
+{
+	int i, ln = strlen(str);
+
+	for (i = 0; i <= ln - 1; i += 2)
+	{
+		printf("%c",str[i] );
+	}
+	printf("\n");
+}
