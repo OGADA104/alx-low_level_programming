69877062c6191d678e01f196aab92cc60598874e
OGADA104 <michaelogada104@gmail.com>
2023-04-01 17:53:42 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*reverse_array - print arrays in reverse
+*@a: input array
+*@n: size if array
+*Return: void
+*
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		printf("%d", a[i]);
+		if (i != 0)
+		{
+			printf(", ");
+		}
+	}
+}
