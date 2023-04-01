d097abe611e39ab43c33321fe2f2d03f7d2ba34c
OGADA104 <michaelogada104@gmail.com>
2023-04-01 13:53:42 +0000

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<string.h>
+
+/**
+**_strncpy - copy string with max
+*number of bytes from src
+*@dest: destination string pointer
+*@src: the string to be copied
+*@n: numbet of bytes in src
+*Return: finished product after copy
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
