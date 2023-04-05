d9c22060f47ce950a6837b5a44b1add42f7381ba
OGADA104 <michaelogada104@gmail.com>
2023-04-05 18:15:27 +0000

    task 0 recusion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*_puts_recursion - prints characters
+*with new line at the end
+*@s: pointer to input string
+*
+*
+*
+*/
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
