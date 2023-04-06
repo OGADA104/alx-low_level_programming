0ac3adf523fca25a68b2f0a3c56e53a614673db4
OGADA104 <michaelogada104@gmail.com>
2023-04-06 19:15:47 +0000

    power of n

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,11 @@
 #include<math.h>
 
 /**
-*
-*
-*
-*
-*
+*_pow_recursion - calc power with recursion
+*@x: base int
+*@y: power value
+*Return: -1 if y < 0,1 if y equals 0
+*calculate power of base x to y
 */
 int _pow_recursion(int x, int y)
 {
