5318afbf3d412dd5323ac578efd6ecb2cb93a56f
OGADA104 <michaelogada104@gmail.com>
2023-04-06 18:21:35 +0000

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+*_print_rev_recursion - prints strings in reverse
+*character by character
+*@s: pointer to string
+*
+*
+*
+*
+*/
+void _print_rev_recursion(char *s)
+{
+	puts(s - 1);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
