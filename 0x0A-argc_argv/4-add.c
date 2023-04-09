47c525e81f7b3eb15fb8fd799813b9ee8bc0260a
OGADA104 <michaelogada104@gmail.com>
2023-04-09 17:55:03 +0000

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+/**
+*main - adds args passed
+*@argc:argument count
+*@argv:argument vector array
+*Return: 0 if success
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, psd;
+
+	for (i = 1; i < argc; i++)
+	{
+		psd = atoi(argv[i]);
+		if (psd > 0)
+		{
+			sum += psd;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
