5d3ce4db0a49777e0979bc3cb663c1b24ea49627
OGADA104 <michaelogada104@gmail.com>
2023-04-12 13:48:18 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+**str_concat - concatenete two str
+*@s1: string 1
+*@s2: string 2
+*Return: pointer to concat string
+*
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *srt12 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+
+	if (srt12 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		strcpy(srt12, s1);
+		strcat(srt12, s2);
+		return (srt12);
+	}
+	free(srt12);
+}
