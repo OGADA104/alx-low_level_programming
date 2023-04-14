5640ba07b0ea88d6f502b3cb4b38c8713d04cd69
MICHAEL <michaelogada104@gmail.com>
2023-04-14 17:51:36 +0000

    task 1 correction more mallocs

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,15 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *join;
 	unsigned int m = n;
 
-	join = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	if (join == NULL)
-		return (NULL);
 	if (n >= strlen(s2))
 		m = strlen(s2);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	join = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	if (join == NULL)
+		return (NULL);
 	strcpy(join, s1);
 	strncat(join, s2, m);
 	return (join);
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i <= (max - min + 1); i++)
+	for (i = 0; i < (max - min + 1); i++)
 	{
 		p[i] = min + 1;
 	}
