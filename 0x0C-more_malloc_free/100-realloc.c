f2ab6ee8f169cb0f290466414f07887bd0d1d156
MICHAEL <michaelogada104@gmail.com>
2023-04-14 20:01:21 +0000

    reallocate memory with new size

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-		ptr = malloc(sizeof(old_size + new_size));
+		ptr = malloc(sizeof(unsigned int) * new_size);
+	if (old_size > new_size)
+	{
+		free(ptr);
+		ptr = malloc(sizeof(unsigned int) * new_size);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
