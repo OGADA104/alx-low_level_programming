5ad56cd988b10aa9675663489822a0d1aacf0cce
MICHAEL <michaelogada104@gmail.com>
2023-04-15 11:24:37 +0000

    free grid correction

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,7 +12,9 @@ void free_grid(int **grid, int height)
 	int i;
 
 	if (*grid == NULL && height == 0)
-		return;
+		exit(0);
+	if (grid == NULL && height > 0)
+		exit(0);
 	if (height > 0 && grid != NULL)
 	{
 		for (i = 0; i < height; i++)
