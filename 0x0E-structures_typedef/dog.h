2cbffe39e4209ff47a594a6ddb0ca3a79ef2e147
MICHAEL <michaelogada104@gmail.com>
2023-04-17 20:57:28 +0000

    struct task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ *struct dog - our dog
+ *@name: name of dog
+ *@age: age of the dog
+ *@owner: the owner of the dog
+ *Description: information of our dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
