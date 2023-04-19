794403479b53c2ff0c51698aec83f591dc715d4f
MICHAEL <michaelogada104@gmail.com>
2023-04-19 20:16:10 +0000

    itaratng pointer functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+#include<stddef.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		exit(0);
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
+}
