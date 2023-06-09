d4208b5274ef6b602c629b482c82abd19f468c07
MICHAEL <michaelogada104@gmail.com>
2023-06-09 10:08:10 +0000

    alx task 1 arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+*main - print no of args
+*@argc: arguments count
+*@argv: arguments passed
+*Return: 0 if success
+*
+*
+*/
+int main(int argc, char *argv[])
+{
+	if (argv[] != NULL)
+	{
+		printf("%d", argc);
+	}
+	printf('\n');
+	return (0);
+}
