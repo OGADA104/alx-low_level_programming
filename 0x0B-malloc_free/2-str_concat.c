f5c595078ca2850a3414183110bea750ea97398a
MICHAEL <michaelogada104@gmail.com>
2023-06-12 18:20:25 +0000

    strconcat task 2 malloc fixed null and betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,10 @@
 #include<string.h>
 #include<stdlib.h>
 /**
- *
- *
- *
- *
+ *char *str_concat - concatenate s1 and s2 to new string
+ *@s1: string 1
+ *@s2: string 2
+ *Return: pointer to new string or NULL
  *
  *
  */
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 
 	if (s1 == NULL)
-		len1 = 0;
+		len1 = 1;
 	else
 		len1 = strlen(s1);
 	if (s2 == NULL)
-		len2 = 0;
+		len2 = 1;
 	else
 		len2 = strlen(s2);
 	s12 = malloc(((sizeof(char) * len1) + (sizeof(char) * len2) + 1));
