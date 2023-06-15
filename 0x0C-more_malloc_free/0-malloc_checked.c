5c7cd6c3721296a1969d4977e665a4cb73c34f51
MICHAEL <michaelogada104@gmail.com>
2023-06-15 19:04:05 +0000

    more malloc allocate memory task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ **malloc_checked - allocate memory using malloc
+ *@b: input argument
+ *Return: pointer or 98 if fails
+ *
+ *
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(sizeof(b));
+	if (p != NULL)
+	{
+		return (p);
+	}
+	exit(98);
+}
