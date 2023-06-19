d4ecf223ed3e91f5cda67f58ba3224c779dbea4b
MICHAEL <michaelogada104@gmail.com>
2023-06-19 19:16:22 +0000

    poppy struct task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structured data for dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: 0wner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
