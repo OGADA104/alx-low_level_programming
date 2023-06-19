dc6623f0aabd2ae0fc6f92bf62a7776d24283107
MICHAEL <michaelogada104@gmail.com>
2023-06-19 18:03:42 +0000

    printfile name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#ifndef FILENAME
+#define FILENAME __FILE__
+#endif
+
+#include<stdio.h>
+
+int main()
+{
+	printf("%s\n", FILENAME);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#ifndef FILENAME
+#define FILENAME __FILE__
+#endif
+
+#include<stdio.h>
+
+/**
+ *main - print file name
+ *Return: 0 if success
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", FILENAME);
+	return (0);
+}
