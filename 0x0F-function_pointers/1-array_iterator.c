cee103c729d98484555a0d3db0947e602bce8521
MICHAEL <michaelogada104@gmail.com>
2023-06-21 20:37:45 +0000

    array iterator task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include<stddef.h>
+#include "function_pointers.h"
+
+/**
+ *array_iterator - itereates through an array
+ *@array: array to iterate through
+ *@size: size of array
+ *@action: function pointer
+ *
+ *
+ *
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
