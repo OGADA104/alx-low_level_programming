d0b0c1ae2c11edd7ceec065782f16b6fcfbba866
MICHAEL <michaelogada104@gmail.com>
2023-08-07 08:32:45 +0000

    malloc args to string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,12 +26,10 @@ char *argstostr(int ac, char **av)
 	{
 		for (i = 0; i < ac; i++)
 		{
-			p[i] = malloc(strlen(av[i]) + 1);
+			p[i] = malloc(strlen(av[i]));
 			strcpy(p[i], av[i]);
 			printf("%s\n", p[i]);
 		}
-		return (p[i]);
 	}
-	return (NULL);
-
+	return (0);
 }
