f7bf3ee57491267f6946316774bea1b050f13333
MICHAEL <michaelogada104@gmail.com>
2023-08-07 10:04:49 +0000

    string to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,37 +1,71 @@
 #include "main.h"
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- *
- *
- *
- *
- *
+ * strtow - split a string into words
+ * @str: input string
+ * Return: pointer to an array of strings (words) or NULL on failure
  */
 char **strtow(char *str)
 {
-	int i, s_len;
-	char **wrds;
-	int *w_len;
+	int i, j, k, s_len, word_count = 0;
+	char **words;
+	int in_word = 0;
 
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL || *str == '\0')
 		return (NULL);
 	s_len = strlen(str);
-	wrds = malloc(s_len + 1);
-	if (wrds != NULL)
+	/* Count the number of words in the input string */
+	for (i = 0; i < s_len; i++)
 	{
+		if (str[i] != ' ' && !in_word)
+		{
+			word_count++;
+			in_word = 1;
+		}
+		else if (str[i] == ' ')
+		{
+			in_word = 0;
+		}
+	}
+	if (word_count == 0)
+		return (NULL);
+	if (word_count == 0)
+		return (NULL);
+	words = malloc(sizeof(char *) * (word_count + 1));
+	if (words != NULL)
+	{
+		in_word = 0;
+		j = 0;
 		for (i = 0; i < s_len; i++)
 		{
-			if (str[i] != " ")
+			if (str[i] != ' ')
 			{
-				w_len[i] = strlen(str[i]);
-				wrds[i] = malloc(w_len[i] + 1);
-				strcpy(wrds[i], str[i]);
-				printf("%s\n", wrds[i]);
+				if (!in_word)
+				{
+					/* Start of a new word */
+					k = i;
+					in_word = 1;
+				}
+			}
+			else if (in_word)
+			{
+				/* End of the word, allocate memory and copy the word */
+				int word_len = i - k;
+				words[j] = malloc(sizeof(char) * (word_len + 1));
+				if (words[j] != NULL)
+				{
+					strncpy(words[j], str + k, word_len);
+					words[j][word_len] = '\0';
+					/* Null-terminate the word */
+					j++;
+					in_word = 0;
+				}
 			}
 		}
-		return (wrds);
+		words[j] = NULL; /* Null-terminate the array of words */
+		return (words);
 	}
 	return (NULL);
 }
